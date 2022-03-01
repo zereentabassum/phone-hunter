bdcf35883223c62d4af733e734c3d663d711e15e
zereentabassum <islamms262@gmail.com>
2022-03-01 12:57:46 +0600

    display result added

--- js/script.js
+++ js/script.js
@@ -3,9 +3,12 @@ const searchPhone = () => {
   const searchText = searchInput.value;
   
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-  console.log(url);
 
   fetch(url)
   .then (res => res.json())
-  .then (data => console.log(data));
+  .then (data => displayResult(data.data));
+}
+
+const displayResult = data => {
+  console.log(data);
 }
