48de960cf8d3cf0325d1c18a125ae1ddb3536a1d
zereentabassum <islamms262@gmail.com>
2022-03-01 16:27:20 +0600

    cards and photos added

--- js/script.js
+++ js/script.js
@@ -10,5 +10,21 @@ const searchPhone = () => {
 }
 
 const displayResult = data => {
-  console.log(data);
+  const searchResult = document.getElementById('search-result');
+  data.forEach(data => {
+    console.log(data);
+ 
+  const div = document.createElement('div');
+  div.classList.add('col');
+  div.innerHTML = `
+      <div class="card">
+      <img src="${data.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      </div>
+  `;
+     searchResult.appendChild(div);
+});
 }
