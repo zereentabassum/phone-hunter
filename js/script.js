b33e60c6b6d17b0fdcd3779f2a54094d5f5ea46a
zereentabassum <islamms262@gmail.com>
2022-03-02 02:02:50 +0600

    phone features added

--- js/script.js
+++ js/script.js
@@ -39,4 +39,25 @@ const displayResult = data => {
 
   const displayPhoneInfo = phone => {
     console.log(phone);
+    const phoneDetails = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card-body text-primary">
+      <h3 class="card-title">${phone.name}</h3>
+      <h5 class="card-title">Chipset: ${phone.mainFeatures.chipSet}</h5>
+      <h5 class="card-title">Display Size: ${phone.mainFeatures.displaySize}</h5>
+      <h5 class="card-title">Memory: ${phone.mainFeatures.memory}</h5>
+      <h5 class="card-title">Storage: ${phone.mainFeatures.storage}</h5>
+      <h5 class="card-title">Bluetooth: ${phone.others.Bluetooth}</h5>
+      <h5 class="card-title">GPS: ${phone.others.GPS}</h5>
+      <h5 class="card-title">NFC: ${phone.others.NFC}</h5>
+      <h5 class="card-title">Radio: ${phone.others.Radio}</h5>
+      <h5 class="card-title">USB: ${phone.others.USB}</h5>
+      <h5 class="card-title">WLN: ${phone.others.WLN}</h5>
+    </div>
+    </div>
+    `;
+    phoneDetails.appendChild(div);
   }
