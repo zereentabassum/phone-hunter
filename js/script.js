20a99e0c0f33342c688850b5f4e889fca72a00cc
zereentabassum <islamms262@gmail.com>
2022-03-01 10:40:02 +0600

    search text and fetch added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-const 
+const searchPhone = () => {
+  const searchInput = document.getElementById('search-input');
+  const searchText = searchInput.value;
+  
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  console.log(url);
+
+  fetch(url)
+  .then (res => res.json())
+  .then (data => console.log(data));
+}
