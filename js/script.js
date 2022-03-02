77cf0a2df682ab01de49968f4a9221906ba040f6
zereentabassum <islamms262@gmail.com>
2022-03-02 19:00:21 +0600

    released date and finishing

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 const searchPhone = () => {
   const searchInput = document.getElementById('search-input');
   const searchText = searchInput.value;
+  // clear data
   searchInput.value = '';
-
+  const searchResult = document.getElementById('phone-details');
+  searchResult.textContent = '';
+  // load data
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  
 
   fetch(url)
   .then (res => res.json())
-  .then (data => displayResult(data.data));
+  .then (data => displayResult(data.data))
 }
-
+  // display result 
 const displayResult = data => {
   const searchResult = document.getElementById('search-result');
     searchResult.textContent = '' ;
   data.forEach(data => {
- 
+
   const div = document.createElement('div');
   div.classList.add('col');
   div.innerHTML = `
@@ -23,14 +27,14 @@ const displayResult = data => {
       <div class="card-body">
         <h5 class="card-title">${data.brand}</h5>
         <h5 class="card-title">${data.phone_name}</h5>
-        <button onclick="loadPhoneInfo('${data.slug}')" class="btn-danger px-4 py-2 rounded">Explore</button>
+        <a  onclick="loadPhoneInfo('${data.slug}')" class="btn btn-danger px-4 py-2" href="#" role="button">Explore</a>
       </div>
       </div>
   `;
      searchResult.appendChild(div);
 });
 }
-
+  // load phone details 
   const loadPhoneInfo = phoneId => {
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
 
@@ -38,16 +42,17 @@ const displayResult = data => {
        .then(res => res.json())
        .then(data => displayPhoneInfo(data.data));
   }
-
+      //  display phone details 
   const displayPhoneInfo = phone => {
-    console.log(phone);
     const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
     <img src="${phone.image}" class="card-img-top" alt="...">
     <div class="card-body text-dark">
       <h3 class="card-title text-primary">${phone.name}</h3>
+      <h4 class="card-title text-primary">${phone.releaseDate ? phone.releaseDate:'No release date found'}</h4>
       <h4 class="card-title text-success">Main Features :</h4>
       
       <h5 class="card-title">Chipset: ${phone.mainFeatures.chipSet}</h5>
